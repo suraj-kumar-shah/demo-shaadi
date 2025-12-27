4270c6ce97ced88087b4dd531f215586d98d66be
suraj-kumar-sah <surajshah72600@gmail.com>
2025-12-28 01:00:15 +0530

    decorate some hero section

---
+++ script.js
@@ -0,0 +1,8 @@
+const navLinks = document.querySelectorAll(".navbar a");
+
+navLinks.forEach(link => {
+  link.addEventListener("click", () => {
+    navLinks.forEach(l => l.classList.remove("active"));
+    link.classList.add("active");
+  });
+});
